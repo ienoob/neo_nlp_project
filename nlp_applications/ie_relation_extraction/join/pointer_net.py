8f30913df85b193b7e2776a90bb261b1fe82ee1b
ienoob <mostli@qq.com>
2021-04-12 22:09:37 +0800

    添加两种关系实体识别模型

---
+++ nlp_applications/ie_relation_extraction/join/pointer_net.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Time : 2021/4/11 19:43
+    @Author  : jack.li
+    @Site    : 
+    @File    : pointer_net.py
+
+    Joint Extraction of Entities and Relations Based on a Novel Decomposition Strategy
+
+"""
+
