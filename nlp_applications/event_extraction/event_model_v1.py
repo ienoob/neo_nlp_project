b4a0410b63acfd1862ae42f31a947c47c09ec52d
ienoob <ie_noob@sina.com>
2021-04-20 15:26:35 +0800

    完成句子级别的事件抽取模型base 模型

--- nlp_applications/event_extraction/event_model_v1.py
+++ nlp_applications/event_extraction/event_model_v1.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) ***
 
-from typing import List
+from typing import List, Callable
 import numpy as np
 import tensorflow as tf
 from nlp_applications.data_loader import LoaderBaiduDueeV1, EventDocument, Event, Argument
 # from transformers import BertTokenizer, TFBertModel, TFBertPreTrainedModel, BertConfig
 
-sample_path = "D:\data\百度比赛\\2021语言与智能技术竞赛：多形态信息抽取任务\句子级事件抽取\\"
+sample_path = "D:\\data\\句子级事件抽取\\"
 bd_data_loader = LoaderBaiduDueeV1(sample_path)
 
 vocab_size = len(bd_data_loader.char2id)
@@ -16,6 +16,7 @@ embed_size = 64
 lstm_size = 64
 event_num = len(bd_data_loader.event2id)
 batch_num = 10
+argument_num = len(bd_data_loader.argument_role2id)
 
 
 def padding_data(input_batch):
@@ -23,25 +24,50 @@ def padding_data(input_batch):
     event_labels = []
     event_trigger_start = []
     event_trigger_end = []
+    trigger_masks = []
+    event_argument_start = []
+    event_argument_end = []
+    event_label_ids = []
     max_len = 0
     for bd in input_batch:
         encodings.append(bd["encoding"])
         event_labels.append(bd["event_label"])
-        max_len = max(max_len, len(bd["encoding"]))
+        max_len = max(max_len, bd["encoding"].shape[0])
+        event_label_ids.append(bd["event_label_id"])
+
     for bd in input_batch:
         trigger_start = np.zeros(max_len)
         trigger_end = np.zeros(max_len)
+
         for s, e, eid in bd["trigger_loc"]:
             trigger_start[s] = eid
             trigger_end[e] = eid
+
+            trigger_mask = np.ones(max_len)*-1e30
+            trigger_mask[s:e+1] = 0
+            trigger_masks.append(trigger_mask)
+
         event_trigger_start.append(trigger_start)
         event_trigger_end.append(trigger_end)
+
+        for argus in bd["event_arguments"]:
+            event_argus_start = np.zeros(max_len)
+            event_argus_end = np.zeros(max_len)
+            for s, e, aid in argus:
+                event_argus_start[s] = aid
+                event_argus_end[s] = aid
+            event_argument_start.append(event_argus_start)
+            event_argument_end.append(event_argus_end)
 
     return {
         "encoding": tf.keras.preprocessing.sequence.pad_sequences(encodings, padding="post"),
         "event_label": tf.cast(event_labels, dtype=tf.int64),
         "event_trigger_start": tf.cast(event_trigger_start, dtype=tf.float32),
-        "event_trigger_end": tf.cast(event_trigger_end, dtype=tf.float32)
+        "event_trigger_end": tf.cast(event_trigger_end, dtype=tf.float32),
+        "event_argument_start": tf.cast(event_argument_start, dtype=tf.float32),
+        "event_argument_end": tf.cast(event_argument_end, dtype=tf.float32),
+        "trigger_masks": tf.cast(event_argument_end, dtype=tf.float32),
+        "event_label_ids": event_label_ids
     }
 
 
@@ -61,6 +87,7 @@ def sample_single_doc(input_doc: EventDocument):
     label_data = np.zeros(event_num)
     trigger_loc = []
     event_arguments = []
+    event_label_id = []
     for e in event_list:
         arguments = []
         label_data[e.id] = 1
@@ -68,13 +95,72 @@ def sample_single_doc(input_doc: EventDocument):
         for e_a in e.arguments:
             arguments.append((e_a.start, e_a.start+len(e_a.argument)-1, bd_data_loader.argument_role2id[e_a.role]))
         event_arguments.append(arguments)
+        event_label_id.append(e.id)
 
     return {
         "encoding": tf.cast(text_id, dtype=tf.int64),
         "event_label": tf.cast(label_data, dtype=tf.int64),
         "trigger_loc": trigger_loc,
-        "event_arguments": event_arguments
+        "event_arguments": event_arguments,
+        "event_label_id": event_label_id
     }
+
+
+# class WarmUp(tf.keras.optimizers.schedules.LearningRateSchedule):
+#
+#     def __init__(
+#         self,
+#         initial_learning_rate: float,
+#         decay_schedule_fn: Callable,
+#         warmup_steps: int,
+#         power: float = 1.0,
+#         name: str = None,
+#     ):
+#         super().__init__()
+#         self.initial_learning_rate = initial_learning_rate
+#         self.warmup_steps = warmup_steps
+#         self.power = power
+#         self.decay_schedule_fn = decay_schedule_fn
+#         self.name = name
+#
+#     def __call__(self, step):
+#         with tf.name_scope(self.name or "WarmUp") as name:
+#             # Implements polynomial warmup. i.e., if global_step < warmup_steps, the
+#             # learning rate will be `global_step/num_warmup_steps * init_lr`.
+#             global_step_float = tf.cast(step, tf.float32)
+#             warmup_steps_float = tf.cast(self.warmup_steps, tf.float32)
+#             warmup_percent_done = global_step_float / warmup_steps_float
+#             warmup_learning_rate = self.initial_learning_rate * tf.math.pow(warmup_percent_done, self.power)
+#             return tf.cond(
+#                 global_step_float < warmup_steps_float,
+#                 lambda: warmup_learning_rate,
+#                 lambda: self.decay_schedule_fn(step - self.warmup_steps),
+#                 name=name,
+#             )
+#
+#     def get_config(self):
+#         return {
+#             "initial_learning_rate": self.initial_learning_rate,
+#             "decay_schedule_fn": self.decay_schedule_fn,
+#             "warmup_steps": self.warmup_steps,
+#             "power": self.power,
+#             "name": self.name,
+#         }
+
+boundaries = [100000, 110000]
+values = [0.1, 0.01, 0.001]
+
+lr_schedule = tf.keras.optimizers.schedules.PiecewiseConstantDecay(boundaries, values)
+
+def repeat_data(input_feature, input_event_label_ids):
+    inner_batch_num = input_feature.shape[0]
+    out_data = []
+    for i in range(inner_batch_num):
+        sub_feature = input_feature[i]
+        for j in input_event_label_ids[i]:
+            out_data.append(sub_feature)
+
+    return tf.cast(out_data, dtype=tf.float32)
 
 
 class UNModel(tf.keras.models.Model):
@@ -83,13 +169,16 @@ class UNModel(tf.keras.models.Model):
         super(UNModel, self).__init__()
         # self.bert = TFBertModel.from_pretrained(config)
         self.embed = tf.keras.layers.Embedding(vocab_size, embed_size)
-        self.lstm = tf.keras.layers.LSTM(lstm_size, return_sequences=True)
+        self.lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(lstm_size, return_sequences=True))
         self.drop_out = tf.keras.layers.Dropout(0.5)
         self.event_classifier = tf.keras.layers.Dense(event_num, activation="sigmoid")
         self.tagger_start = tf.keras.layers.Dense(event_num, activation="softmax")
         self.tagger_end = tf.keras.layers.Dense(event_num, activation="softmax")
 
-    def call(self, inputs, training=None, mask=None):
+        self.argument_start = tf.keras.layers.Dense(argument_num, activation="softmax")
+        self.argument_end = tf.keras.layers.Dense(argument_num, activation="softmax")
+
+    def call(self, inputs, trigger_masks=None, event_label_ids=None, training=None, mask=None):
         x = self.embed(inputs)
         seq = self.lstm(x)
 
@@ -99,10 +188,18 @@ class UNModel(tf.keras.models.Model):
         tagger_start = self.tagger_start(seq)
         tagger_end = self.tagger_end(seq)
 
-        return event_label, tagger_start, tagger_end
+        m = tf.expand_dims(trigger_masks, axis=-1)
+        argument_embed = repeat_data(seq, event_label_ids)
+        mask_argument = argument_embed+m
+
+        argument_feature = tf.concat([argument_embed, mask_argument], axis=2)
+        argument_start = self.argument_start(argument_feature)
+        argument_end = self.argument_end(argument_feature)
+        return event_label, tagger_start, tagger_end, argument_start, argument_end
+
 
 um_model = UNModel()
-optimizer = tf.keras.optimizers.Adam()
+optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 # loss = tf.keras.losses.mean_squared_error()
 
 sample_predict = tf.constant([[0.9, 0.5]])
@@ -111,14 +208,18 @@ sample_label = tf.constant(([[1, 0]]))
 # print(tf.keras.losses.MSE(sample_label, sample_predict))
 loss_func = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-def train_step(input_xx, input_yy, input_trigger_start, input_trigger_end):
+
+def train_step(input_xx, input_yy, input_trigger_mask, input_event_label_ids, input_trigger_start, input_trigger_end,
+               input_argument_start, input_argument_end):
 
     with tf.GradientTape() as tape:
-        logits1, logits2, logits3  = um_model(input_xx)
+        logits1, logits2, logits3, logits4, logits5 = um_model(input_xx, input_trigger_mask, input_event_label_ids)
         lossv1 = tf.reduce_mean(tf.keras.losses.MSE(input_yy, logits1))
         lossv2 = loss_func(input_trigger_start, logits2)
         lossv3 = loss_func(input_trigger_end, logits3)
-        lossv = lossv1 + lossv2 + lossv3
+        lossv4 = loss_func(input_argument_start, logits4)
+        lossv5 = loss_func(input_argument_end, logits5)
+        lossv = lossv1 + lossv2 + lossv3 + lossv4 + lossv5
 
     variables = um_model.variables
     gradients = tape.gradient(lossv, variables)
@@ -133,8 +234,13 @@ for ep in range(epoch):
 
     for batch, data in enumerate(get_batch_data(batch_num)):
         loss_value = train_step(data["encoding"], data["event_label"],
+                                data["trigger_masks"],
+                                data["event_label_ids"],
                                 data["event_trigger_start"],
-                                data["event_trigger_end"])
+                                data["event_trigger_end"],
+                                data["event_argument_start"],
+                                data["event_argument_end"]
+                                )
 
         if batch % 10 == 0:
             print("epoch {0} batch {1} loss is {2}".format(ep, batch, loss_value))
