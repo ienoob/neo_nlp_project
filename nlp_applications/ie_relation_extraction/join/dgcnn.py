74a4d1da2e7bc7e6c1a7fbd4935ae67c6dacf127
ienoob <mostli@qq.com>
2021-04-29 21:39:03 +0800

    测试dgcnn 方法

--- nlp_applications/ie_relation_extraction/join/dgcnn.py
+++ nlp_applications/ie_relation_extraction/join/dgcnn.py
@@ -8,7 +8,8 @@
 
 """
 import tensorflow as tf
-from nlp_applications.layers.dilated_gated_conv1d import dilated_gated_conv1d
+from nlp_applications.layers.dilated_gated_conv1d import dilated_gated_conv1d, DilatedGatedConv1d
+
 
 class DGCNN(tf.keras.Model):
 
@@ -16,14 +17,15 @@ class DGCNN(tf.keras.Model):
         super(DGCNN, self).__init__()
 
         self.embed = tf.keras.layers.Embedding(100, 64)
+        self.dgc = DilatedGatedConv1d(64)
 
     def call(self, inputs, training=None, mask=None):
 
         x = self.embed(inputs)
-        x = dilated_gated_conv1d(x, mask)
+        mask = tf.cast(tf.greater(tf.expand_dims(inputs, 2), 0), dtype=tf.float32)
+        x = self.dgc([x, mask])
 
         return x
-
 
 
 sample_data = tf.constant([[1, 2, 3, 0, 0]])
@@ -31,4 +33,5 @@ sample_mask = tf.constant([[1, 1, 1, 0, 0]])
 
 dgcnn = DGCNN()
 
-print(dgcnn(sample_data, sample_mask))
+out = dgcnn(sample_data, sample_mask)
+print(out)
