800e76565084191de7940de62b6e7566f50121cf
ienoob <mostli@qq.com>
2021-05-01 11:25:21 +0800

    新增阅读理解模型

---
+++ nlp_applications/qa_system/mrc_model_v1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Time : 2021/5/1 10:10
+    @Author  : jack.li
+    @Site    : 
+    @File    : mrc_model_v1.py
+
+"""
+
+from nlp_applications.data_loader import LoaderDuReaderChecklist
+
+train_path = "D:\data\百度比赛\\2021语言与智能技术竞赛：机器阅读理解任务"
+data_loader = LoaderDuReaderChecklist(train_path)
