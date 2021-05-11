dc5b5e3f5c3cf950f1a1a44723b672210eb2c969
ienoob <mostli@qq.com>
2021-05-11 18:02:51 +0800

    complete the process

--- nlp_applications/ner/pattern_model.py
+++ nlp_applications/ner/pattern_model.py
@@ -44,6 +44,32 @@ logger.info("word2vec is load")
 
 
 # 严格结果， 要求完成重合
+def hard_score_res_v2(real_data, predict_data):
+    assert len(real_data) == len(predict_data)
+
+    score = 0.0
+    d_count = 0
+    p_count = 0
+    for i, rd in enumerate(real_data):
+        if len(predict_data[i]) is 0:
+            continue
+        p_count += len(predict_data[i])
+        d_count += len(real_data[i])
+
+        for rd_ele in rd:
+            if rd_ele in predict_data[i]:
+                score += 1
+    matrix = {
+        "score": score,
+        "p_count": p_count,
+        "d_count": d_count,
+        "recall": (score+1e-8)/(d_count+1e-3),
+        "precision": (score + 1e-8) / (p_count + 1e-3)
+    }
+
+    return matrix
+
+
 def hard_score_res(real_data, predict_data):
     assert len(real_data) == len(predict_data)
 
@@ -102,9 +128,55 @@ def soft_score_res(real_data, predict_data):
     return matrix
 
 
+def soft_score_res_v2(real_data, predict_data):
+    assert len(real_data) == len(predict_data)
+    score = 0.0
+    d_count = 0
+    p_count = 0
+
+    def tiny_score(i_real, i_pres):
+        r_s_ind = i_real[0]
+        r_e_ind = i_real[0] + len(i_real[1])
+        p_s_ind = i_pres[0]
+        p_e_ind = i_pres[0] + len(i_pres[1])
+        if r_s_ind > p_s_ind or r_e_ind < p_e_ind:
+            mix_area = 0.0
+        else:
+            mix_area = min(r_e_ind, p_e_ind) - max(r_s_ind, p_s_ind)
+
+        all_area = max(r_e_ind, p_e_ind) - min(r_s_ind, p_s_ind)
+
+        return mix_area/all_area
+
+    for i, rd in enumerate(real_data):
+        pre_res = predict_data[i]
+        if len(pre_res) is 0:
+            continue
+        p_count += len(pre_res)
+        d_count += len(rd)
+        for rd_ele in rd:
+            max_score = 0.0
+            for pd_ele in pre_res:
+                e_score = tiny_score(rd_ele, pd_ele)
+                if e_score > max_score:
+                    max_score = e_score
+            score += max_score
+
+    matrix = {
+        "score": score,
+        "p_count": p_count,
+        "d_count": d_count,
+        "recall": (score + 1e-8) / (d_count + 1e-3),
+        "precision": (score + 1e-8) / (p_count + 1e-3)
+    }
+
+    return matrix
+
+
 def sample_data(input_event_type, input_event_role, data_source):
-    data_list = []
+    train_data_list = []
     text_list = []
+    label_list = []
     for data in data_source:
         arguments = set()
         for event in data["event_list"]:
@@ -113,12 +185,13 @@ def sample_data(input_event_type, input_event_role, data_source):
             for arg in event["arguments"]:
                 if arg["role"] != input_event_role:
                     continue
-                data_list.append((data["text"], arg["argument_start_index"], arg["argument"]))
+                train_data_list.append((data["text"], arg["argument_start_index"], arg["argument"]))
                 arguments.add((arg["argument_start_index"], arg["argument"]))
         if arguments:
-            text_list.append((data["text"], list(arguments)))
+            text_list.append(data["text"])
+            label_list.append(arguments)
 
-    return data_list, text_list
+    return train_data_list, text_list, label_list
 
 
 def bm25(query, doc_list):
@@ -183,12 +256,14 @@ def single_pattern(input_str):
 def cosine_distance(input_a, input_b):
     a_norm = np.linalg.norm(input_a)
     b_norm = np.linalg.norm(input_b)
+    if a_norm == 0 or b_norm == 0:
+        return 0.0
     similarity = np.dot(input_a, input_b.T) / (a_norm * b_norm)
 
     return similarity
 
 
-tree_shreshold = 0.8
+tree_shreshold = 0.9
 
 
 class PatternModel(object):
@@ -419,17 +494,44 @@ class PatternModel(object):
         assert len(self.word_embed_feature) > 0
         res_ind = -1
         res_score = -1
+        inner_threshold = 0.999
+        filter_span = []
         for iv_ind, span_feature in enumerate(input_span_feature):
             score = max([cosine_distance(span_feature, cmp_embed) for cmp_embed in self.word_embed_feature])
-            if score > res_score:
-                res_score = score
-                res_ind = iv_ind
-        return res_ind, res_score
+            filter_span.append((iv_ind, score))
+            # if score > res_score:
+            #     res_score = score
+            #     res_ind = iv_ind
+        filter_span.sort(key=lambda x: x[1], reverse=True)
+        filter_span_score = [(k, v) for k, v in filter_span if v>inner_threshold]
+        if len(filter_span_score):
+            return filter_span_score[:3]
+        else:
+            return filter_span[:1]
+
+    def calculate_classifier_score(self, input_span_feature):
+        extract_span_feature = self.generate_feature(input_span_feature)
+        extract_span_res = self.clf.predict_proba(extract_span_feature)
+
+        filter_span = []
+        inner_threshold = 0.8
+        for i, span_res in enumerate(extract_span_res[:,1]):
+
+            filter_span.append((i, span_res))
+        filter_span.sort(key=lambda x: x[1], reverse=True)
+        filter_span_score = [(k, v) for k, v in filter_span if v>inner_threshold]
+
+        if len(filter_span_score):
+            return filter_span_score
+        else:
+            return filter_span[:1]
+
+
 
     def predict(self, input_text):
         predict_res = []
         for text in input_text:
-            extract = tuple()
+            extract_list = []
             extract_infos = dict()
             for pt in self.pattern_list:
                 try:
@@ -457,20 +559,23 @@ class PatternModel(object):
             # extract_infos.sort(key=lambda x: x[2], reverse=True)
             extract_infos_reverse = {v: k for k, v in extract_infos.items()}
             if extract_infos:
-                inx = 0
                 extract_span_list = [e_info[1] for e_info in extract_infos]
                 extract_span_feature = self.generate_feature(extract_span_list)
-                # extract_span_res = self.clf.predict_proba(extract_span_feature)
-                # inx = np.argmax(extract_span_res[:,1])
-                assert len(extract_span_feature) > 0
-                inx, match_score = self.calculate_word_embed_sim(extract_span_feature)
+                # extract_span_res = self.calculate_classifier_score(extract_span_list)
 
-                if inx == -1:
-                    inx = 0
-                print("max {0} score is {1} ".format(extract_infos_reverse[inx][1], match_score))
+                extract_span_res = self.calculate_word_embed_sim(extract_span_feature)
 
-                extract = (extract_infos_reverse[inx][0], extract_infos_reverse[inx][1])
-            predict_res.append(extract)
+                # assert len(extract_span_res) > 0
+                # print(len(extract_span_res))
+                # match_span =
+                # print(len(match_span))
+
+                # inx = 0
+                # print("max {0} score is {1} ".format(extract_infos_reverse[inx][1], match_score))
+                for inx, e_score in extract_span_res:
+                    extract = (extract_infos_reverse[inx][0], extract_infos_reverse[inx][1])
+                    extract_list.append(extract)
+            predict_res.append(extract_list)
 
         return predict_res
 
@@ -511,26 +616,26 @@ if __name__ == "__main__":
         for role in schema["role_list"]:
             role_value = role["role"]
 
-            test_train, _ = sample_data(event_type, role_value, train_data)
-            test_eval, _ = sample_data(event_type, role_value, eval_data)
+            test_train, test_train_data, test_train_label = sample_data(event_type, role_value, train_data)
+            test_eval, test_eval_data, test_eval_label = sample_data(event_type, role_value, eval_data)
             print("event ", event_type, " start, role ", role_value, " start, train data, ", len(test_train))
             if len(test_train) == 0:
                 continue
             test_train_feature = [d[0] for d in test_train]
-            test_train_label = [(d[1],d[2]) for d in test_train]
+            test_for_train_label = [(d[1], d[2]) for d in test_train]
 
-            test_eval_feature = [d[0] for d in test_eval]
-            test_eval_label = [(d[1], d[2]) for d in test_eval]
+            # test_eval_feature = [d[0] for d in test_eval]
+            # test_eval_label = [(d[1], d[2]) for d in test_eval]
             pt = PatternModel()
-            pt.fit(test_train_feature, test_train_label)
-            train_pres = pt.predict(test_train_feature)
-            eval_pres = pt.predict(test_eval_feature)
+            pt.fit(test_train_feature, test_for_train_label)
+            train_pres = pt.predict(test_train_data)
+            eval_pres = pt.predict(test_eval_data)
 
-            train_hard_eval = hard_score_res(test_train_label, train_pres)
-            train_soft_eval = soft_score_res(test_train_label, train_pres)
+            train_hard_eval = hard_score_res_v2(test_train_label, train_pres)
+            train_soft_eval = soft_score_res_v2(test_train_label, train_pres)
 
-            eval_hard_eval = hard_score_res(test_eval_label, eval_pres)
-            eval_soft_eval = soft_score_res(test_eval_label, eval_pres)
+            eval_hard_eval = hard_score_res_v2(test_eval_label, eval_pres)
+            eval_soft_eval = soft_score_res_v2(test_eval_label, eval_pres)
 
             calculate_result(train_eval_dict, train_hard_eval, train_soft_eval)
             calculate_result(eval_eval_dict, eval_hard_eval, eval_soft_eval)
