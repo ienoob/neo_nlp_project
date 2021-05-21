b668f536933ff21ddc5d3c79655265349c62c6e2
ienoob <mostli@qq.com>
2021-05-21 23:16:01 +0800

    完善multi head 模型

--- nlp_applications/ie_relation_extraction/join/multi_head.py
+++ nlp_applications/ie_relation_extraction/join/multi_head.py
@@ -15,7 +15,8 @@ import tensorflow as tf
 char_size = 10
 char_embed = 10
 word_embed = 10
-
+entity_num = 10
+rel_num = 10
 
 
 class MultiHeaderModel(tf.keras.Model):
@@ -26,9 +27,26 @@ class MultiHeaderModel(tf.keras.Model):
         self.char_embed = tf.keras.layers.Embedding(char_size, char_embed)
         self.word_embed = tf.keras.layers.Embedding(char_size, char_embed)
 
-    def call(self, encoding, entity_label, training=None, mask=None):
-        mask = None
-        char_embed = self.char_embed(encoding)
+        self.bi_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(10, return_sequences=True))
+
+        self.emission = tf.keras.layers.Dense(entity_num)
+
+        self.crf = None
+        self.selection_u = tf.keras.layers.Dense(rel_num)
+        self.selection_v = tf.keras.layers.Dense(rel_num)
+        self.selection_uv = tf.keras.layers.Dense(rel_num)
+
+
+
+    def call(self, char_ids, word_ids, training=None, mask=None):
+        mask_value = tf.not_equal(char_ids, 0)
+        char_embed = self.char_embed(char_ids)
+        word_embed = self.word_embed(word_ids)
+
+        embed = tf.concat([char_embed, word_embed], axis=-1)
+        sent_encoder = self.bi_lstm(embed)
+
+
 
 
 
