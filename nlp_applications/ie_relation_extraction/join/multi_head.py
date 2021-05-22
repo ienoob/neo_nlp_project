210790ba81054a3e6bc402a86e1d9be505b23a46
ienoob <mostli@qq.com>
2021-05-22 23:01:40 +0800

    完善multi head 模型v2

--- nlp_applications/ie_relation_extraction/join/multi_head.py
+++ nlp_applications/ie_relation_extraction/join/multi_head.py
@@ -26,6 +26,7 @@ class MultiHeaderModel(tf.keras.Model):
 
         self.char_embed = tf.keras.layers.Embedding(char_size, char_embed)
         self.word_embed = tf.keras.layers.Embedding(char_size, char_embed)
+        self.ent_embed = tf.keras.layers.Embedding()
 
         self.bi_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(10, return_sequences=True))
 
@@ -37,14 +38,35 @@ class MultiHeaderModel(tf.keras.Model):
         self.selection_uv = tf.keras.layers.Dense(rel_num)
 
 
-
-    def call(self, char_ids, word_ids, training=None, mask=None):
+    def call(self, char_ids, word_ids, entity_ids, training=None, mask=None):
         mask_value = tf.not_equal(char_ids, 0)
         char_embed = self.char_embed(char_ids)
         word_embed = self.word_embed(word_ids)
 
         embed = tf.concat([char_embed, word_embed], axis=-1)
         sent_encoder = self.bi_lstm(embed)
+        eimission = self.emission(sent_encoder)
+
+        ent_encoder = self.ent_emb(entity_ids)
+
+        rel_encoder = tf.concat((sent_encoder, ent_encoder), axis=-1)
+        B, L, H = rel_encoder.shape
+        u = tf.expand_dims(self.selection_u(rel_encoder), axis=1)
+        v = tf.expand_dims(self.selection_v(rel_encoder), axis=2)
+        uv = self.selection_uv(tf.concat((u, v), axis=-1))
+
+        selection_logits = tf.einsum('bijh,rh->birj', uv, self.rel_emb.weight)
+
+def test_run_model():
+
+    model = MultiHeaderModel()
+    sample_char_id = tf.constant([[1, 2]])
+    sample_word_id = tf.constant([[2, 3]])
+    sample_entity_id = tf.constant([[1, 2]])
+
+
+
+
 
 
 
