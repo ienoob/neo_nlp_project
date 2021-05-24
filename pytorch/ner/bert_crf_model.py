184d4dcc72d839278fbe8f546ce4e222db741651
ienoob <mostli@qq.com>
2021-05-24 20:18:41 +0800

    wan shang pytorch crf + bert implement

--- pytorch/ner/bert_crf_model.py
+++ pytorch/ner/bert_crf_model.py
@@ -1,13 +1,88 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Copyright (c) ***
+import numpy as np
 import torch
 import torch.nn as nn
-from transformers.modeling_bert import BertModel
+from torch.optim import Optimizer
+from transformers import BertModel
 from pytorch.layers.crf import CRF
 import torch.autograd as autograd
 import torch.optim as optim
+from pytorch.layers.bert_optimization import BertAdam
+from transformers import BertTokenizer
+from nlp_applications.data_loader import LoadMsraDataV2
 
+msra_data = LoadMsraDataV2("D:\data\\ner\\msra_ner_token_level\\")
+bert_model_name = "bert-base-chinese"
+class_num = len(msra_data.label2id)
+
+
+def sequence_padding(inputs, length=None, padding=0, is_float=False):
+    """Numpy函数，将序列padding到同一长度
+    """
+    if length is None:
+        length = max([len(x) for x in inputs])
+
+    outputs = np.array([
+        np.concatenate([x, [padding] * (length - len(x))])
+        if len(x) < length else x[:length] for x in inputs
+    ])
+
+    out_tensor = torch.FloatTensor(outputs) if is_float \
+        else torch.LongTensor(outputs)
+    return torch.tensor(out_tensor)
+
+
+class DataIterator(object):
+    def __init__(self, input_loader, input_batch_num):
+        self.input_loader = input_loader
+        self.input_batch_num = input_batch_num
+        self.entity_label2id = {"O": 1, "pad": 0}
+        self.max_len = 0
+        self.tokenizer = BertTokenizer.from_pretrained(bert_model_name)
+
+    def _transformer2feature(self, sentence, label_list):
+        sentence_id = self.tokenizer.encode(sentence)
+        label_id = [self.input_loader.label2id[label_i] for label_i in label_list]
+        label_id = [1] + label_id + [0]
+        sentence_mask = [1 for _ in sentence_id]
+
+        return {
+            "sentence_id": sentence_id,
+            "label_id": label_id,
+            "sentence_mask": sentence_mask
+        }
+
+    def batch_transformer(self, input_batch_data):
+        batch_sentence_id = []
+        batch_label_id = []
+        batch_sentence_mask = []
+        for data in input_batch_data:
+            batch_sentence_id.append(data["sentence_id"])
+            batch_label_id.append(data["label_id"])
+            batch_sentence_mask.append(data["sentence_mask"])
+
+        batch_sentence_id = sequence_padding(batch_sentence_id, length=512)
+        batch_label_id = sequence_padding(batch_label_id, length=512)
+        batch_sentence_mask = sequence_padding(batch_sentence_mask, length=512)
+
+        return {
+            "sentence_id": batch_sentence_id,
+            "label_id": batch_label_id,
+            "sentence_mask": batch_sentence_mask
+        }
+
+    def __iter__(self):
+        inner_batch_data = []
+        for i, sentence in enumerate(self.input_loader.train_sentence_list):
+            tf_data = self._transformer2feature(sentence, self.input_loader.train_tag_list[i])
+            inner_batch_data.append(tf_data)
+            if len(inner_batch_data) == self.input_batch_num:
+                yield self.batch_transformer(inner_batch_data)
+                inner_batch_data = []
+        if inner_batch_data:
+            yield self.batch_transformer(inner_batch_data)
 
 class BertNER(nn.Module):
     def __init__(self, gpu_use=False, bertpath=None, label_alphabet_size=2, dropout=0.5):
@@ -35,12 +110,12 @@ class BertNER(nn.Module):
     def get_tags(self, batch_bert, bert_mask):
         seg_id = torch.zeros(bert_mask.size()).long().cuda() if self.gpu else torch.zeros(bert_mask.size()).long()
         outputs = self.bert_encoder(batch_bert, bert_mask, seg_id)
-        outputs = outputs[0][:, 1:-1, :]
+        outputs = outputs[0]
         tags = self.hidden2tag(outputs)
 
         return tags
 
-    def neg_log_likelihood_loss(self, word_inputs, biword_inputs, word_seq_lengths, mask, batch_label, batch_bert,
+    def neg_log_likelihood_loss(self, mask, batch_label, batch_bert,
                                 bert_mask):
         tags = self.get_tags(batch_bert, bert_mask)
 
@@ -57,10 +132,77 @@ class BertNER(nn.Module):
         return tag_seq
 
 
-input_data = None
-bert_model = BertNER(input_data)
-parameter = bert_model.parameters()
-optimizer = optim.Adamax(parameter)
 
+MAX_SENTENCE_LENGTH = 512
+
+def lr_decay(optimizer, epoch, decay_rate, init_lr):
+    lr = init_lr * ((1 - decay_rate) ** epoch)
+    print(" Learning rate is setted as:", lr)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return optimizer
+
+
+
+def predict_check(pred_variable, gold_variable, mask_variable):
+    """
+        input:
+            pred_variable (batch_size, sent_len): pred tag result, in numpy format
+            gold_variable (batch_size, sent_len): gold result variable
+            mask_variable (batch_size, sent_len): mask variable
+    """
+
+    pred = pred_variable.cpu().data.numpy()
+    gold = gold_variable.cpu().data.numpy()
+    mask = mask_variable.cpu().data.numpy()
+    overlaped = (pred == gold)
+    right_token = np.sum(overlaped * mask)
+    total_token = mask.sum()
+
+    return right_token, total_token
+
+
+if __name__ == "__main__":
+    # input_file = ""
+    # bertpath = ""
+    iteration = 5
+    train_Ids = []
+    warm_up = True
+    HP_lr = 0.0015
+    HP_batch_size = 10
+    data_iterator = DataIterator(msra_data, HP_batch_size)
+
+    model = BertNER(bertpath=bert_model_name, label_alphabet_size=class_num)
+    # parameter = bert_model.parameters()
+
+
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = optim.Adamax(parameters)
+    if warm_up:
+        param_optimizer = list(model.named_parameters())
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in param_optimizer if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+            {'params': [p for n, p in param_optimizer if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+        ]
+        num_train_optimization_steps = int(len(train_Ids) / HP_batch_size) * iteration
+        optimizer = BertAdam(optimizer_grouped_parameters,
+                             lr=HP_lr,
+                             warmup=0.1,
+                             t_total=num_train_optimization_steps)
+    # else:
+    #     optimizer = lr_decay(optimizer, idx, data.HP_lr_decay, data.HP_lr)
+
+    for idx, batch_data in enumerate(data_iterator):
+
+        model.train()
+        model.zero_grad()
+
+        loss, tag_seq = model.neg_log_likelihood_loss(batch_data["sentence_mask"].byte(),
+                                                      batch_data["label_id"],
+                                                      batch_data["sentence_id"],
+                                                      batch_data["sentence_mask"])
+        print(loss)
+        right, whole = predict_check(tag_seq, batch_data["label_id"], batch_data["sentence_mask"])
 
 
