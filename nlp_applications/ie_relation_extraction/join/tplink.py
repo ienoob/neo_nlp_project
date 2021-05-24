1e1bd27fc6a0291396ef8bdff41aeed04e48b5b4
ienoob <mostli@qq.com>
2021-05-24 23:35:45 +0800

    添加tplink 算法

---
+++ nlp_applications/ie_relation_extraction/join/tplink.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Time : 2021/5/24 23:31
+    @Author  : jack.li
+    @Site    : 
+    @File    : tplink.py
+
+    TPLinker: Single-stage Joint Extraction of Entities and Relations Through Token Pair Linking
+
+"""
+import tensorflow as tf
+
+class Tplink(tf.keras.Model):
+
+    def __init__(self):
+        super(Tplink, self).__init__()
+
+
