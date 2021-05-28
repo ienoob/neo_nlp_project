0d43fe04de024b74d4ac54b711d9f88f900fa635
ienoob <mostli@qq.com>
2021-05-28 19:03:04 +0800

    完善match lstm 模型

--- nlp_applications/qa_system/match_lstm.py
+++ nlp_applications/qa_system/match_lstm.py
@@ -7,9 +7,10 @@
     @File    : match_lstm.py
 
     Wang, Shuohang, and Jing Jiang. "Machine comprehension using match-lstm and answer pointer.
+    github https://github.com/laddie132/Match-LSTM
 """
 import tensorflow as tf
-from nlp_applications.data_loader import LoaderDuReaderChecklist
+from nlp_applications.data_loader import LoaderDuReaderChecklist, BaseDataIterator
 
 train_path = "D:\data\百度比赛\\2021语言与智能技术竞赛：机器阅读理解任务"
 data_loader = LoaderDuReaderChecklist(train_path)
@@ -18,18 +19,26 @@ vocab_size = len(data_loader.char2id)
 embed_size = 64
 lstm_size = 64
 
-class DataIter(object):
+
+class DataIter(BaseDataIterator):
 
     def __init__(self, input_data_loader):
         self.data_loader = input_data_loader
 
 
-
 class MatchInteraction(tf.keras.layers.Layer):
+    """
+    interaction context and question with attention mechanism
+    """
     def __init__(self):
         super(MatchInteraction, self).__init__()
 
         self.left_match_rnn = None
+
+
+class MatchRNN(tf.keras.layers.Layer):
+    def __init__(self):
+        super(MatchRNN, self).__init__()
 
 
 class MatchLSTM(tf.keras.Model):
@@ -37,15 +46,18 @@ class MatchLSTM(tf.keras.Model):
     def __init__(self):
         super(MatchLSTM, self).__init__()
         self.embed = tf.keras.layers.Embedding(vocab_size, embed_size)
-        self.context_lstm = tf.keras.layers.LSTM(lstm_size)
-        self.q_lstm = tf.keras.layers.LSTM(lstm_size)
+        self.encoder = tf.keras.layers.LSTM(lstm_size)
+        self.match_rnn = None
+        # self.q_lstm = tf.keras.layers.LSTM(lstm_size)
 
     def call(self, input_context, input_q, training=None, mask=None):
         context_embed = self.embed(input_context)
         question_embed = self.embed(input_q)
 
-        context_encoder = self.context_lstm(context_embed)
-        q_encoder = self.q_lstm(question_embed)
+        context_encoder = self.encoder(context_embed)
+        q_encoder = self.encoder(question_embed)
+
+
 
 
 
