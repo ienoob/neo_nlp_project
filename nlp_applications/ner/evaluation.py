f787d254cf6e3860da97d032ed97883bd893d539
ienoob <mostli@qq.com>
2021-06-02 00:17:03 +0800

    增加评估计算方法

--- nlp_applications/ner/evaluation.py
+++ nlp_applications/ner/evaluation.py
@@ -36,6 +36,19 @@ def extract_entity(input_label):
     return extract_ner
 
 
+def eval_metrix(hit_num, true_num, predict_num):
+    recall = (hit_num + 1e-8) / (true_num + 1e-3)
+    precision = (hit_num + 1e-8) / (predict_num + 1e-3)
+    f1_value = 2 * recall * precision / (recall + precision)
+
+    return {
+        "recall": recall,
+        "precision": precision,
+        "f1_value": f1_value
+    }
+
+
+
 # 这里输入的BIO类型的数据
 def metrix(true_labels, predict_labels):
     true_res = 0
