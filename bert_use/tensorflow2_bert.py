3506223dbdf9055e8c886c4ecf564c1f360bd6e1
ienoob <mostli@qq.com>
2021-06-05 22:38:54 +0800

    完善tensorflow bert 训练

--- bert_use/tensorflow2_bert.py
+++ bert_use/tensorflow2_bert.py
@@ -11,31 +11,40 @@ import tensorflow.compat.v1 as tf
 from bert_use.bert_vtf2 import modeling
 tf.disable_v2_behavior()
 
-bert_config_file = "D:\data\\bert\\cased_L-12_H-768_A-12\cased_L-12_H-768_A-12\\bert_config.json"
-init_checkpoint = "D:\data\\bert\\cased_L-12_H-768_A-12\cased_L-12_H-768_A-12\\bert_model.ckpt"
+bert_config_file = "D:\data\\bert\\cased_L-12_H-768_A-12\\cased_L-12_H-768_A-12\\bert_config.json"
+init_checkpoint = "D:\data\\bert\\cased_L-12_H-768_A-12\\cased_L-12_H-768_A-12\\bert_model.ckpt"
 
-input_ids = tf.constant([[31, 51, 99], [15, 5, 0]], name="test1")
-input_mask = tf.constant([[1, 1, 1], [1, 1, 0]], name="test2")
-token_type_ids = tf.constant([[0, 0, 1], [0, 2, 0]], name="test3")
+inputs_seq = tf.placeholder(shape=[None, None], dtype=tf.int32, name="inputs_seq") # B * (S+2)
+inputs_mask = tf.placeholder(shape=[None, None], dtype=tf.int32, name="inputs_mask") # B * (S+2)
+inputs_segment = tf.placeholder(shape=[None, None], dtype=tf.int32, name="inputs_segment")
 
 bert_config = modeling.BertConfig.from_json_file(bert_config_file)
 # bert_config = modeling.BertConfig(vocab_size=32000, hidden_size=512,
 #     num_hidden_layers=8, num_attention_heads=6, intermediate_size=1024)
 
-model = modeling.BertModel(config=bert_config, is_training=False,
-    input_ids=input_ids, input_mask=input_mask, token_type_ids=token_type_ids)
-
+model = modeling.BertModel(config=bert_config, is_training=True,
+    input_ids=inputs_seq, input_mask=inputs_mask, token_type_ids=inputs_segment,
+                           use_one_hot_embeddings=False)
+# init = tf.global_variables_initializer()
 tvars = tf.trainable_variables()
-
 (assignment_map, initialized_variable_names) = modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
-
 tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
+for var in tvars:
+    print(var)
+    init_string = ""
+    if var.name in initialized_variable_names:
+        init_string = ", *INIT_FROM_CKPT*"
+    tf.logging.info("  name = %s, shape = %s%s", var.name, var.shape,
+                    init_string)
+# saver = tf.train.Saver()
+# ckpt = tf.train.get_checkpoint_state(FLAGS.restore_model)
+# saver.restore(sess, ckpt.model_checkpoint_path)
+tf_config = tf.ConfigProto(allow_soft_placement=True)
+with tf.Session(config=tf_config) as sess:
+    sess.run(tf.global_variables_initializer())
 
-pooled_output = model.get_pooled_output()
-print(model.all_encoder_layers)
-# class TF2Bert(tf.keras.models.Model):
-#
-#     def __init__(self):
-#         super(TF2Bert, self).__init__()
-#
-#         self.bert =
+    # res = sess.run(tvars[10])
+    print(tvars[10].get_weights())
+    # print(tvars[10])
+    # print(res[0][0])
+
