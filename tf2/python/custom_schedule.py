166135d634a4379182e47216ba743fae40d2c8ec
ienoob <mostli@qq.com>
2021-06-13 22:46:09 +0800

    增加学习率调节器

---
+++ tf2/python/custom_schedule.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+    @Time : 2021/6/13 22:43
+    @Author  : jack.li
+    @Site    : 
+    @File    : tf2lr.py
+
+"""
+import tensorflow as tf
+
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+    def __init__(self, d_model, warmup_steps=4000):
+        super(CustomSchedule, self).__init__()
+
+        self.d_model = tf.cast(d_model, tf.float32)
+        self.warmup_steps = warmup_steps
+
+    def __call__(self, step):
+        arg1 = tf.math.rsqrt(step)
+        arg2 = step * (self.warmup_steps ** -1.5)
+
+        return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
