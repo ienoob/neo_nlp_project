009e160861a262f502ee0dc85a9faeb344df5346
ienoob <mostli@qq.com>
2021-07-23 18:25:28 +0800

    新增模型评估结果

--- nlp_applications/ner/evaluation.py
+++ nlp_applications/ner/evaluation.py
@@ -187,3 +187,15 @@ def soft_score_res_v2(real_data, predict_data):
     }
     matrix["f1_value"] = 2 * matrix["recall"] * matrix["precision"] / (matrix["recall"] + matrix["precision"])
     return matrix
+
+
+def eval_metrix_v3(hit_num, true_num, predict_num):
+    recall = (hit_num + 1e-8) / (true_num + 1e-3)
+    precision = (hit_num + 1e-8) / (predict_num + 1e-3)
+    f1_value = 2 * recall * precision / (recall + precision)
+
+    return {
+        "recall": recall,
+        "precision": precision,
+        "f1_value": f1_value
+    }
