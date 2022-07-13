f47f15392164791f77cc328ed4255e1746502e97
ienoob <mostli@qq.com>
2022-07-13 11:58:19 +0800

    删除数据v3

--- pytorch/event_extraction/bert_utils.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# Copyright (c) ***
-"""
-    bert 相关工具
-
-"""
-
-split_cha = {" ", "\n", "\u2003", "\u200b", "\ufeff", "\xa0", "\u3000", "\x06", "\x07", "\u2002"}
